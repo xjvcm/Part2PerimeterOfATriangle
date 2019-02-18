cdea0a438c3e1e3f0ea276520636288126c731ec
Yanni App13 <jonvincruman@gmail.com>
2019-02-17 19:04:54 -0800

    added prompt and ifelse statement to validate inputs

--- Triangle.java
+++ Triangle.java
@@ -14,6 +14,30 @@ import java.util.Scanner;
 
 public class Triangle {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        Scanner input = new Scanner(System.in);
+
+        // Declare variables for triable sides & perimeter
+        double sideOne, sideTwo, sideThree, perimeter;
+
+        // Prompt user to input lenght of each side
+        System.out.println("Enter lengths of sides of the triangle: ");
+        System.out.print("Side 1: ");
+        sideOne = input.nextDouble();
+        System.out.print("Side 2: ");
+        sideTwo = input.nextDouble();
+        System.out.print("Side 3: ");
+        sideThree = input.nextDouble();
+
+        // Validate inputs
+        if ((sideOne + sideTwo) <= sideThree || (sideOne + sideThree 
+            <= sideTwo) || (sideTwo + sideThree) <= sideOne) {
+                System.out.println("Those sides do not specify a valid "
+                + "triangle.");
+        }
+        else {
+            perimeter = sideOne + sideTwo + sideThree;
+            System.out.println("The perimeter of the triangle is " 
+                + perimeter);
+        }
     }
 }
