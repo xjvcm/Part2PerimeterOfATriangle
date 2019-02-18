42765487a44b9dfee9da0f2eefcd773868c177b2
Yanni App13 <jonvincruman@gmail.com>
2019-02-17 18:30:47 -0800

    created header and declared class

---
+++ Triangle.java
@@ -0,0 +1,19 @@
+/*
+    Jonathan Manzano
+    
+    Assignment: Selection Structures - Part 2: Perimeter of a Triangle
+
+    This program is to calculate the perimeter of a triangle.  The user is
+    prompt to input the lenght of each side.  An input validation is 
+    implemented to ensure that the lenghts entered equate to a valid 
+    triangle.  For this validation, sum of two sides should result in being
+    greater than the side that's left over.
+*/
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
